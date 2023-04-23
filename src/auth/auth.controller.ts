06ee954a7c6a2cfa437c465cbd4a19a119d304cb
pierslabs <pedrolosasp@gmail.com>
2023-04-23 11:15:45 +0200

    add endpoint gethola

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,28 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { Auth } from './entities/auth.entity';
-import { AuthGuard } from '@nestjs/passport';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Req,
+} from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { CreateAuthDto } from "./dto/create-auth.dto";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { Auth } from "./entities/auth.entity";
+import { AuthGuard } from "@nestjs/passport";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-
-  @Post('signup')
-  signIn(@Body() authCredentialsDto: AuthCredentialsDto):Promise<{accessToken: string}> {
+  @Post("signup")
+  signIn(
+    @Body() authCredentialsDto: AuthCredentialsDto
+  ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
   }
 
   @Post()
-  create(@Body() createAuthDto: CreateAuthDto):Promise<void> {
+  create(@Body() createAuthDto: CreateAuthDto): Promise<void> {
     return this.authService.createUser(createAuthDto);
   }
 
-  @Post('/test')
-  @UseGuards(AuthGuard('jwt'))
+  @Post("/test")
+  @UseGuards(AuthGuard("jwt"))
   async testJwt(@Req() req) {
-    console.log(req.user)
+    console.log(req.user);
+  }
+
+  @Get("/testChanges")
+  async getHola() {
+    return "hola todo funciona!!!";
   }
 }
