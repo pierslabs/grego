83a2e5306dea3bdd5066eaeab390684c2df607de
pierslabs <pedrolosasp@gmail.com>
2023-04-23 11:03:04 +0200

    change credentials db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
-
+// conf postgres
 @Module({
   imports: [
     AuthModule,
