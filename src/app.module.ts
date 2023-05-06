e883259d5018a284b33a5b5335eed5fb73f6de36
pierslabs <pedrolosasp@gmail.com>
2023-05-06 08:45:58 +0200

    add postgre

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
+import { ConfigModule } from '@nestjs/config';
+import { PostgreDBConf } from './conf/postgres.conf';
+
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot(), PostgreDBConf(), AuthModule, UsersModule],
 })
 export class AppModule {}
