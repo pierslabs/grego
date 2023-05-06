949f00105e1da23ed5a1735709eb0504e3d1028c
pierslabs <pedrolosasp@gmail.com>
2023-05-06 18:22:15 +0200

    Auth Service - add users.service in auth.module / auth.service - register.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,25 +1,28 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
+
 import { ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('login')
-  login(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.login(createAuthDto);
-  }
+  // TODO
+  // @Post('login')
+  // login(@Body() createAuthDto: CreateAuthDto) {
+  //   return this.authService.login(createAuthDto);
+  // }
 
   @Post('register')
-  register(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.register();
+  register(@Body() createUserDto: CreateUserDto) {
+    return this.authService.register(createUserDto);
   }
 
-  @Get('revalidate')
-  revalidateToken(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.register();
-  }
+  // TODO
+  // @Get('revalidate')
+  // revalidateToken() {
+  //   return this.authService.revalidateToken();
+  // }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,35 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { User } from 'src/users/entities/user.entity';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
-  login(createAuthDto: CreateAuthDto) {
-    console.log({ createAuthDto });
-    return createAuthDto;
+  constructor(private readonly usersService: UsersService) {}
+  logger = new Logger('Auth Service');
+
+  // TODO
+  // login(createAuthDto: CreateAuthDto) {
+  //   console.log({ createAuthDto });
+  //   return createAuthDto;
+  // }
+
+  async register(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const user = await this.usersService.create(createUserDto);
+      return user;
+    } catch (error) {
+      this.logger.error('💣', error);
+      throw new InternalServerErrorException();
+    }
   }
 
-  register() {
-    return `This action returns all auth`;
-  }
-
-  revalidateToken() {
-    return `This action {revalidate} returns new token`;
-  }
+  // TODO
+  // revalidateToken() {
+  //   return `This action {revalidate} returns new token`;
+  // }
 }
