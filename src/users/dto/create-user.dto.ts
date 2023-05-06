c223de29d1cba8adaf931e932993775b04cc8488
pierslabs <pedrolosasp@gmail.com>
2023-05-06 18:18:20 +0200

    add create user-dto &&  user entity email add  unique

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,31 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty({
+    type: String,
+    description: 'User name',
+    minimum: 2,
+    maximum: 50,
+  })
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
+  name: string;
+
+  @ApiProperty({ type: String, description: 'User email' })
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'User password',
+    minimum: 6,
+    maximum: 50,
+  })
+  @IsString()
+  @MinLength(6)
+  @MaxLength(50)
+  password: string;
+}
