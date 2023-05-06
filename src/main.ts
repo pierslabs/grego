c1b091f9c9602f0a108cd17960a38a3acf79a29e
pierslabs <pedrolosasp@gmail.com>
2023-05-06 07:33:47 +0200

    add auth resource  && swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Grego Auth Service')
+    .setDescription('Authorization JWT')
+    .setVersion('1.0')
+    .addTag('Grego')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3001);
 }
 bootstrap();
